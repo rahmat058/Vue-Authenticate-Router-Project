06e1cfb1a652aa98acafeac71136f67af78481a6
rahmat058 <md.kazi.rahmat@gmail.com>
2019-03-09 10:09:51 +0600

    Validation and route authentication is done

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import Vuelidate from 'vuelidate'
 
 import router from './router'
 import store from './store'
 
-axios.defaults.baseURL = 'https://vue-router-e3601.firebaseio.com'
-//axios.defaults.headers.common['Authorization'] = 'abcdefg'
+Vue.use(Vuelidate)
+
+axios.defaults.baseURL = 'https://vue-router-e3601.firebaseio.com/'
+// axios.defaults.headers.common['Authorization'] = 'fasfdsa'
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
 const reqInterceptor = axios.interceptors.request.use(config => {
-  console.log('Request Interceptors: ', config)
+  console.log('Request Interceptor', config)
   return config
 })
-
 const resInterceptor = axios.interceptors.response.use(res => {
-  console.log('Response Interceptors: ', res)
+  console.log('Response Interceptor', res)
   return res
 })
 
